9c7a8025d0dc424957aa63259677c6d88ad0f891
Arabinda Kumar Nayak <133951645+ArabindakumarNayak@users.noreply.github.com>
2024-12-13 21:40:46 +0530

    13/12-1st commit

--- Employee-Management-System/src/main/java/com/pronix/employeeManagement/config/SecurityConfig.java
+++ Employee-Management-System/src/main/java/com/pronix/employeeManagement/config/SecurityConfig.java
@@ -2,16 +2,33 @@ package com.pronix.employeeManagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+//public class SecurityConfig {
+//
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//		return httpSecurity.csrf(c->c.disable()).build();
+//	}
+//	
+//	
+//}
 
 @Configuration
 public class SecurityConfig {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
-		
-	}
-	
-	
+    @Bean 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(c->c.disable())
+        	.httpBasic(Customizer.withDefaults())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/saveEmployee", "/getEmployeeById/**").permitAll() 
+                .anyRequest().authenticated()
+            )
+            .formLogin(); // Enables form-based login
+        return http.build();
+    }
 }
