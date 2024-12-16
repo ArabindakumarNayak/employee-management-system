baebb3439d27842e5ca2bab9357c392b3e32b53f
Arabinda Kumar Nayak <133951645+ArabindakumarNayak@users.noreply.github.com>
2024-12-16 10:33:45 +0530

    16/12/24

--- Employee-Management-System/src/main/java/com/pronix/employeeManagement/config/SecurityConfig.java
+++ Employee-Management-System/src/main/java/com/pronix/employeeManagement/config/SecurityConfig.java
@@ -3,7 +3,10 @@ package com.pronix.employeeManagement.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 //@Configuration
@@ -31,4 +34,21 @@ public class SecurityConfig {
             .formLogin(); // Enables form-based login
         return http.build();
     }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public AuthenticationManagerBuilder configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("ashok").password("ashok@123").authorities("admin").and()
+            .withUser("raju").password("raju@123").authorities("read").and()
+            .withUser("mahesh").password("mahesh@123").authorities("read");
+        return auth;
+    }
+    
+    
+    
 }
