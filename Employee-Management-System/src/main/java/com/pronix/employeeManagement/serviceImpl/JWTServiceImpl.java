b1f52d0254568a194e595e5fffb02f51def8b819
Arabinda Kumar Nayak <133951645+ArabindakumarNayak@users.noreply.github.com>
2024-12-20 10:07:05 +0530

    20/12/2024 - 10:06

--- Employee-Management-System/src/main/java/com/pronix/employeeManagement/serviceImpl/JWTServiceImpl.java
+++ Employee-Management-System/src/main/java/com/pronix/employeeManagement/serviceImpl/JWTServiceImpl.java
@@ -40,8 +40,15 @@ public class JWTServiceImpl implements JWTServive {
 	public String generateToken(String username) {
 
 		Map<String, Object> claims = new HashMap<>();
-		return Jwts.builder().claims().add(claims).subject(username).issuedAt(new Date(System.currentTimeMillis()))
-				.expiration(new Date(System.currentTimeMillis() + 60 * 60 * 100)).and().signWith(getKey()).compact();
+		return Jwts.builder()
+				.claims()
+				.add(claims)
+				.subject(username)
+				.issuedAt(new Date(System.currentTimeMillis()))
+				.expiration(new Date(System.currentTimeMillis() + 60 * 60 * 100))
+				.and()
+				.signWith(getKey())
+				.compact();
 	}
 
 	private SecretKey getKey() {
